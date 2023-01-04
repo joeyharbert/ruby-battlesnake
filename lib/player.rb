ae79bfc15c3da2b926457df446a1d6bebbd90f7a
Jaime Bellmyer <online@bellmyer.com>
2023-01-03 20:13:19 -0600

    path find to food within 6 blocks

--- lib/player.rb
+++ lib/player.rb
@@ -3,7 +3,7 @@ require 'battlesnake'
 class Player
   attr_reader :data, :board, :player, :logger
 
-  DIRECTIONS = ['up', 'down', 'left', 'right']
+  MAX_PATH_DISTANCE = 6
 
   def initialize(data, logger)
     @data = data
@@ -21,15 +21,30 @@ class Player
 
   def available_directions
     return @available_directions if defined?(@available_directions)
-    @available_directions = board.available_directions(player.head).shuffle
+    @available_directions = board.available_directions(player.head)
+  end
+
+  def random_directions
+    available_directions.shuffle
   end
 
   def nearby_food
-    available_directions.detect{ |location| board.food?(location) }
+    direction_to(nearest_path_to(board.food))
+  end
+
+  def nearest_path_to(locations)
+    Array(locations).map do |location|
+      board.find_path(player.head, location, max_distance: MAX_PATH_DISTANCE)
+    end.compact.min_by(&:size)
+  end
+
+  def direction_to(path)
+    return nil if path.nil?
+    path[0].direction(path[1])
   end
 
   def find_empty
-    available_directions.first
+    random_directions.first
   end
 
   # def nearest_available_wall
